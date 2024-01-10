2f3f52eda23fc5962bcae476cd213f4ed8713561
Chance Carr <chancecarr1@gmail.com>
2024-01-10 11:55:40 -0700

    started project in pyCharm

--- lab/lab01/lab01.py
+++ lab/lab01/lab01.py
@@ -1,9 +1,4 @@
 def eighteen_seventy_five():
-    """Come up with the most creative expression that evaluates to 1875,
-    using only numbers and the +, *, and - operators.
-
-    >>> eighteen_seventy_five()
-    1875
-    """
-    return ______
-    
+    print(1875+1)
+    return
+eighteen_seventy_five()
